14d2ec7ca3c57514895fc54f16f1c7e6b4464625
dingmaotech <yangshuang812@foxmail.com>
2015-07-14 15:47:46 +0800

    Revert "文件删除"
    This reverts commit 08ce062625bbedd92d01281ca1be7e86e4745a65.

--- mvcDemo/src/com/dingmao/mvcdemo/student/service/StudentService.java
+++
@@ -1,8 +0,0 @@
-package com.dingmao.mvcdemo.student.service;
-
-import com.dingmao.mvcdemo.student.model.Student;
-import com.dingmao.platform.service.BaseService;
-
-public interface StudentService extends BaseService<Student, String> {
-	
-}
