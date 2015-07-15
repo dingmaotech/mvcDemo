5b758a1846cebb1d0ac8c333a04e9b350e629646
dingmaotech <yangshuang812@foxmail.com>
2015-07-15 16:03:23 +0800

    重新上传mvcDemo工程

--- code/mvcDemo/src/com/dingmao/platform/dao/base/DaoInterface.java
+++
@@ -1,4 +0,0 @@
-package com.dingmao.platform.dao.base;
-
-public interface DaoInterface<T, PK> {
-}
--- code/mvcDemo/src/com/dingmao/platform/vo/datatables/wrapper/model/AoColumnDefs.java
+++
@@ -1,9 +0,0 @@
-package com.dingmao.platform.vo.datatables.wrapper.model;
- 
-public class AoColumnDefs extends AoColumnVo {
-
-	public Integer aTargets[];
-
-	public AoColumnDefs() {
-	}
-}
